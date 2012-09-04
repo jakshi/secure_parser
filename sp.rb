586ed1bd64b9017b3bcee8dc59aab3578cba610c
Konstantin Lysenko <konstantin@atalanta-systems.com>
2012-09-04 21:55:24 +0300

    I got data that I needed from secure logs, so this is final version, may be will use it us template for other simple log paresers

--- sp.rb
+++ sp.rb
@@ -6,6 +6,7 @@ keys = ['pam_unix\(sshd\:session\)\: session opened for user ', 'pam_unix\(sshd\
 keys_join = keys.product(users)
 
 phrases = []
+results = []
 
 keys_join.each do |key|
   phrases << key.join
@@ -13,7 +14,22 @@ end
 
 phrases.each do |phrase|
   logs = File.open(ARGV[0],"r")
-  logs.grep(/#{phrase}/) {|s| puts s}
+  logs.grep(/#{phrase}/) {|s| results << s}
   logs.close
 end
 
+result2 = []
+
+results.each do |line|
+  log_line = {}
+  /(?<ldate>.*)\ phx\-demo\ sshd\[(?<lproc>\d+)\].*session\ (?<lstate>[closedpen]+)\ for\ user\ (?<luser>\w+)/ =~ line
+  log_line[:date] = ldate
+  log_line[:proc] = lproc
+  log_line[:state] = lstate
+  log_line[:user] = luser
+  result2 << log_line
+#  puts "date - #{log_line[:date]}, proc - #{log_line[:proc]}, state - #{log_line[:state]}, user - #{log_line[:user]}"
+end
+
+sorted = result2.sort_by {|line| line[:proc]}
+sorted.each { |res| p res }
